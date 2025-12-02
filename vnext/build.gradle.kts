f0c9de69040ab262d21d52e0b01ba43a1b8db090
Pasqual Koschmieder <git@derklaro.dev>
2025-12-02 14:01:17 +0100

    chore: update vnext module to 1.21.11-pre4

--- vnext/build.gradle.kts
+++ vnext/build.gradle.kts
@@ -20,9 +20,9 @@ plugins {
   alias(libs.plugins.fabricLoom)
 }
 
-val minecraftVersion = "1.21.11-pre3"
+val minecraftVersion = "1.21.11-pre4"
 val parchmentVersion = "2025.10.05"
-val supportedVersionRange = "1.21.11-pre3"
+val supportedVersionRange = "1.21.11-pre4"
 
 dependencies {
   shaded(projects.common)
